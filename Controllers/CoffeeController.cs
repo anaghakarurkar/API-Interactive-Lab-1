6caa8a51dd16c143d7bdd012d4ab2c2716ff1fda
anaghakarurkar <anaghakarurkar@gmail.com>
2022-10-03 21:17:20 +0100

    Created new class CoffeeController with Get method implementation.

---
+++ Controllers/CoffeeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace API_Interactive_Lab_1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CoffeeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "";
+        }
+    }
+}
